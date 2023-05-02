d8679f7f826113985c5c5797c58c2efdab0ccfc7
WikiFamillemaintainer <122366189+WikiFamillemaintainer@users.noreply.github.com>
2023-05-02 12:42:45 +0200

    Update prototype.hpp

--- prototype.hpp
+++ prototype.hpp
@@ -1,7 +1,45 @@
 #include "math.hpp"
-int ajouteDeux(int nombreRecu)
-{
-    int valeur(nombreRecu + 2);
+#ifndef MATH_HPP_INCLUDED
+#define MATH_HPP_INCLUDED
+int ajouteDeux(int nombreRecu);
+#endif // MATH_HPP_INCLUDED
+#include <iostream>
 
-    return valeur;
+using namespace std;
+
+
+
+   
+
+
+
+// Main
+int main()
+    
+   //Déclaration d'un flux permettant d'écrire dans le fichier
+   // C:/Nanoc/scores.txt
+   return 0;
+// Prototype avec les valeurs par défaut
+int nombreDeSecondes(int heures, int minutes = 0, int secondes = 0);
+// Prototype avec les valeurs par défaut
+int nombreDeSecondes(int heures, int minutes = 0, int secondes = 0);
+// Prototype avec les valeurs par défaut
+int nombreDeSecondes(int heures, int minutes = 0, int secondes = 0);
+
+{
+    cout << nombreDeSecondes(1, 10, 25) << endl;
+
+    return 0;
+}
+
+// Définition de la fonction, SANS les valeurs par défaut
+int nombreDeSecondes(int heures, int minutes, int secondes)
+{
+    int total = 0;
+
+    total = heures * 60 * 60;
+    total += minutes * 60;
+    total += secondes;
+
+    return total;
 }
